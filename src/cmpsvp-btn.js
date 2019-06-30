9248ecff245abc88f5162ffddf1beb19c13a873c
Thomas Tortorini <thomastortorini@gmail.com>
2019-07-01 00:06:38 +0200

    btn

---
+++ src/cmpsvp-btn.js
@@ -0,0 +1,24 @@
+"use strict";
+
+class CMPSVPButton extends HTMLButtonElement  {
+	constructor() {
+		super();
+		this.classList.add( "cmpsvp-btn" );
+		this.innerHTML = `<span class="cmpsvp-btn-text">${ this.innerHTML }</span>`;
+		this.addEventListener( "click", this._onclick.bind( this ) );
+		Object.seal( this );
+
+		cmpsvpRippleFxInit( this );
+	}
+
+	// private:
+	_onclick( e ) {
+		const cl = this.classList;
+
+		if ( cl.contains( "cmpsvp-btn-disable" ) || cl.contains( "cmpsvp-btn-loading" ) ) {
+			e.stopImmediatePropagation();
+		}
+	}
+}
+
+customElements.define( "cmpsvp-btn", CMPSVPButton, { extends: "button" } );
